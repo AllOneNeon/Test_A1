e280953bd468890bc8ab6e6fc054d45260599824
n.o.a <96257435+AllOneNeon@users.noreply.github.com>
2022-07-07 18:04:24 +0300

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -2,7 +2,6 @@ phones = ["Xiaomi Redmi Note 10S", "Смартфон Xiaomi Redmi Note 10 Pro",
          "Apple iPhone 13", "Apple iPhone 11",
           "Huawei nova Y70", "Смартфон Apple iPhone 13 Pro"]
 
-### Первый вариант ###
 value = "Apple"
 value_apple = []
 
